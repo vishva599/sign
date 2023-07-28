e2f582801743b9f9c95f38de7a0331996e8a7040
Vishva <vishvakanani56@gmail.com>
2023-07-28 14:34:54 +0530

    msg

---
+++ src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-auth',
+  templateUrl: './seller-auth.component.html',
+  styleUrls: ['./seller-auth.component.css']
+})
+export class SellerAuthComponent {
+    constructor(){}
+
+    ngOnInit() {}
+    signUp(data:object): void {
+      console.warn(data)
+    }
+}
